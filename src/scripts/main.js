aa03a640ac1e3dd90339ce7f05bd1d6ba2ea08eb
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-07 14:40:28 +0300

    add task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,42 +7,47 @@ all.forEach((form) => {
   const inputs = form.querySelectorAll('input');
 
   inputs.forEach((element) => {
-    const label = document.createElement('label');
-
-    label.className = 'field-label';
-
     let inputId = element.id;
 
-    if (!element.id) {
+    if (!inputId) {
       inputId = `${element.name}idea`;
       element.id = inputId;
     }
-    label.setAttribute('for', inputId);
 
-    const inputName = element.name;
-    let newName = '';
-    let previousChar = '';
+    const existingLabel = element.parentElement.querySelector(
+      `label.field-label[for='${inputId}']`,
+    );
 
-    for (const char of inputName) {
-      if (
-        char === char.toUpperCase() &&
-        previousChar !== ' ' &&
-        newName !== ''
-      ) {
-        newName += ` ${char}`;
-      } else if (char === '_' || char === '-') {
-        newName += ' ';
-      } else {
-        newName += char;
+    if (!existingLabel) {
+      const label = document.createElement('label');
+
+      label.className = 'field-label';
+      label.setAttribute('for', inputId);
+
+      const inputName = element.name;
+      let newName = '';
+      let previousChar = '';
+
+      for (const char of inputName) {
+        if (
+          char === char.toUpperCase() &&
+          previousChar !== ' ' &&
+          newName !== ''
+        ) {
+          newName += ` ${char}`;
+        } else if (char === '_' || char === '-') {
+          newName += ' ';
+        } else {
+          newName += char;
+        }
+        previousChar = char;
       }
-      previousChar = char;
+
+      newName = newName[0].toUpperCase() + newName.slice(1);
+      label.textContent = `${newName}`;
+      element.placeholder = `${newName}`;
+
+      element.parentElement.insertBefore(label, element);
     }
-
-    newName = newName[0].toUpperCase() + newName.slice(1);
-    label.textContent = `${newName}`;
-
-    element.placeholder = `${newName}`;
-
-    element.parentElement.insertBefore(label, element);
   });
 });
