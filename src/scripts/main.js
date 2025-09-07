97472672cf502b9e5ac07d7ea504252d8d61cfcc
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-07 13:49:51 +0300

    testing task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const list = document.getElementsByTagName('form');
+const all = [...list];
+
+all.forEach((form) => {
+  const inputs = form.querySelectorAll('input');
+
+  inputs.forEach((element) => {
+    const label = document.createElement('label');
+
+    label.className = 'field-label';
+    label.setAttribute('for', `${element.name}idea`);
+    element.id = `${element.name}idea`;
+
+    const inputName = element.name;
+    let newName = '';
+
+    for (const char of inputName) {
+      if (char === char.toUpperCase()) {
+        newName += ` ${char}`;
+      } else {
+        newName += char;
+      }
+    }
+
+    newName = newName[0].toUpperCase() + newName.slice(1);
+    label.textContent = `${newName}`;
+
+    element.placeholder = `${newName}`;
+
+    element.parentElement.insertBefore(label, element);
+  });
+});
